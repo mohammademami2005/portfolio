864626299be4574e37c20d06ac567b5b21f8f4d3
Mohammad Emami <162024353+mohammademami2005@users.noreply.github.com>
2025-12-22 02:38:34 +0330

    update sitemap from ts to js

---
+++ public/next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://www.mohammademamiproject.ir/',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 1.0,
+};
+
+module.exports = config;
